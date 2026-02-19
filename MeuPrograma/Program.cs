40a2043fdc801cbcb16c04ecdc515db3215433ee
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-02-19 15:40:41 +0000

    Added a comment to the Class

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -5,9 +5,12 @@ namespace MeuPrograma
     /// <summary>
     /// Principal Class of the program
     /// </summary>
-    /// <param name="args"></param>
     public class Program
     {
+        /// <summary>
+        /// Runs the program
+        /// </summary>
+        /// <param name="args">Arguments passed to the program</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
